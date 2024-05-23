239d07600be19e111bcd5f7d6e75257025d5a7fe
cel3ntato <adriano2024celentano@gmail.com>
2024-05-23 03:25:34 +0200

    structure

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './js/pixabay-api.js';
+import './js/render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
